2067d8198f865bd0e5e0c5f6efd4340fd8fe2dcd
Samarth Kumar <samarthk2402@gmail.com>
2024-07-28 12:51:09 +0100

    changed to "./todo-list-app/"

--- todo-list-app/vite.config.js
+++ todo-list-app/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ToDoApp/todo-list-app/dist",
+  base: "./todo-list-app/",
 });
